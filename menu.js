ca293d75e328c36a54cc3447facd46f2c099d081
Mafe0003 <mafe0003@stud.kea.dk>
2025-01-02 16:30:01 +0100

    step7

--- menu.js
+++ menu.js
@@ -20,3 +20,27 @@ document.querySelector(".kontakt").addEventListener("click", function (e) {
   e.preventDefault(); // Forhindrer standard linkadfærd
   document.querySelector("#scroll").scrollIntoView({ behavior: "smooth" });
 });
+
+//  SCROLL NED TIL TEMA3
+document.querySelector(".scroll2").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector("#tema2").scrollIntoView({ behavior: "smooth" });
+});
+
+//  SCROLL NED TIL TEMA3
+document.querySelector(".scroll3").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector("#tema3").scrollIntoView({ behavior: "smooth" });
+});
+
+//  SCROLL NED TIL TEMA4
+document.querySelector(".scroll4").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector("#tema4").scrollIntoView({ behavior: "smooth" });
+});
+
+//  SCROLL NED TIL TEMA5
+document.querySelector(".scroll5").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector("#tema5").scrollIntoView({ behavior: "smooth" });
+});
